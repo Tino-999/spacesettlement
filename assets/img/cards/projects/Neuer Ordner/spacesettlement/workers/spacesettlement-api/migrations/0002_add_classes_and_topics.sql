f7c07de742bba78b0977d6c83dd97850649b8e54
Tino Schuldt <tinoschuldt100@gmail.com>
2026-01-05 09:42:13 +0100

    clean up

--- assets/img/cards/projects/Neuer Ordner/spacesettlement/workers/spacesettlement-api/migrations/0002_add_classes_and_topics.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE items ADD COLUMN project_class TEXT;
-ALTER TABLE items ADD COLUMN fiction_class TEXT;
-ALTER TABLE items ADD COLUMN topics TEXT;
--- assets/img/cards/projects/Neuer Ordner/spacesettlement/workers/spacesettlement-api/migrations/0004_add_budget.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE items ADD COLUMN budgetBillionUSD REAL;
